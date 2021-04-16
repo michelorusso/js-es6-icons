e82bf68bba2e3759056e2b44b3cd0b46624439cb
Michele Lorusso <kirito102095@hotmail.it>
2021-04-16 19:16:36 +0200

    Milestone 1 completato

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 const icons = [
 	{
 		name: 'cat',
@@ -122,3 +124,41 @@ const colors = [
 	</div>
 </div> 
 */
+
+// Milestone 1
+// iconsContainer -> salviamo il div icons-container nella variabile
+const iconsContainer = $('#icons-container');
+
+printIcons(icons, iconsContainer);
+
+// FUNCTIOS
+// printIcons -> funzione di stampa dell'array di oggetti
+// 
+// iconsArray -> è l'argomento dell'array di oggetti Icons
+// container -> è l'argomento collegato all'HTML( iconsContainer )
+function printIcons(iconsArray, container) {
+	
+	// Usiamo il forEach per ciclare l'array di oggetti
+	iconsArray.forEach((element) => {
+
+		// con il destructuring su element prendiamo le chiavi che ci servono 
+		const {name, prefix, family} = element;
+
+		// iconHtml -> Template Literal per creare il codice che verrà inserito nel container
+		const iconHtml = `
+		<div class="icon">
+			<i class="${family} ${prefix}${name}"></i>
+			<div>
+				${name.toUpperCase()}
+			</div>
+		</div> 
+		`;
+
+		// stampiamo in HTML tutte le icone usando .append
+		container.append(iconHtml);
+		
+	});
+}
+
+
+});
